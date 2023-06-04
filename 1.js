492a36eb4a5609ce36dd6e65a2a006547a493ecf
Murtuza Shaikh <77818735+mkshaikh15@users.noreply.github.com>
2023-06-03 20:28:56 -0400

    Solved Project Euler Problem 1

--- 1.js
+++ 1.js
@@ -1,1 +1,11 @@
+function multiplesOf3and5(number) {
+  var output = 0;
+  for (var i = number-1; i > 0; i--) {
+    if (i%3 == 0 || i%5 == 0) {
+      output += i;
+    }
+  }
+  return output;
+}
 
+console.log(multiplesOf3and5(1000));
